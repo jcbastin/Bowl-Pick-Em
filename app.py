989e7525f44d0ff365416266c34424eaf0766caa
Joshua Bastin <jcbastin@ucla.edu>
2025-12-01 12:38:23 -0800

    Disable scheduler to stop API calls

--- app.py
+++ app.py
@@ -520,7 +520,7 @@ def picks_board():
 # ---------- MAIN ---------- #
 
 # Start scheduler on all deployments (including Gunicorn)
-start_scheduler()
+# start_scheduler()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
